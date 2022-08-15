551399274b39748899877eccc8f438a686f08e69
xndcmd <xndcmd@gmail.com>
2022-08-15 11:47:45 -0700

    initialized ReactJS - server runs successfully

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
-import React from "react";
-import PortfolioContainer from "./components/PortfolioContainer";
+import logo from './logo.svg';
+import './App.css';
 
-const App = () => <PortfolioContainer />;
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
 
 export default App;
