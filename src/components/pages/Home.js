8c0229ea668a01ef215efef68b87a46d6fd82c94
xndcmd <xndcmd@gmail.com>
2022-08-16 17:52:53 -0700

    added photos to all app cards

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import spacesurfer from './space-surfer.png'
+import tcg from './img/teamcardgen.png'
+import figjam from './img/figjam.png'
+import gbooks from './img/booksearch.png'
+import pour from './img/findpour.png'
+import awty from './img/arewethereyet.png'
+import orgchart from './img/orgchartgen.png'
 
 export default function Home() {
   return (
@@ -15,15 +21,15 @@ export default function Home() {
       <p>Here are links to some of my gitHub projects:</p>
       <div class="flex">
 
-        <a href="https://github.com/xnd0/Team_Card_Generator_10.3">
+      <a href="https://github.com/xnd0/CmdLine_OrgChart_12.1">
         <section class="app-card">
-          <div class="app-title"> NodeJS Team Card Generator </div>
+          <div class="app-title"> Command Line Org Chart Generator </div>
           <section>
             <div class="app-img app-card-content">
-              img here
+            <img src={orgchart} alt="orgchart generator screenshot" class="app-img"/>
             </div>
             <p class="light-text smaller-text">
-            A command line application - to record employee information from a software engineering team and generate an HTML file that displays their info. (since this program runs entirely in the terminal via node.js, redirect to repo)
+            A command line orgchart generator - to manage an employee database. It uses Node.js, Inquirer, and MySQL. (Since this program runs entirely in the terminal via node.js, redirect to repo)
             </p>
           </section>
         </section>
@@ -34,7 +40,7 @@ export default function Home() {
           <div class="app-title"> FigJam </div>
           <section>
             <div class="app-img app-card-content">
-              img here
+            <img src={figjam} alt="figjam app screenshot" class="app-img"/>
             </div>
             <p class="light-text">
             A website for musical people - signup, login, post, browse, listen, & watch user's favorite music tracks and videos.
@@ -48,7 +54,7 @@ export default function Home() {
           <div class="app-title"> Custom GoogleBooks API Search </div>
           <section>
             <div class="app-img app-card-content">
-              img here
+            <img src={gbooks} alt="google booksearch screenshot" class="app-img"/>
             </div>
             <p class="light-text">
             An app to search google books API - allows users to create an account to save titles for later. User must be logged in to save books to their account.
@@ -62,7 +68,7 @@ export default function Home() {
           <div class="app-title"> Find Your Pour </div>
           <section>
             <div class="app-img app-card-content">
-              img here
+            <img src={pour} alt="find your pour screenshot" class="app-img"/>
             </div>
             <p class="light-text">
             A website for wine professionals & enthusiasts - wineries may register and post information about their wines, to be displayed on the 'complete wine list'. 
@@ -76,28 +82,29 @@ export default function Home() {
           <div class="app-title"> Are We There Yet </div>
           <section>
             <div class="app-img app-card-content">
-              img here
+            <img src={awty} alt="are we there yet screenshot" class="app-img"/>
             </div>
             <p class="light-text">
-            An app for adventure - search by state to discover national parks and landmarks. Results are return on the mainpage map. Save favorite places to remember for later.
+            An app for adventure - search by state to discover national parks and landmarks. Results are returned on the mainpage map. Save favorite places to remember for later.
             </p>
           </section>
         </section>
         </a>
 
-        <a href="https://github.com/xnd0/CmdLine_OrgChart_12.1">
+        <a href="https://github.com/xnd0/Team_Card_Generator_10.3">
         <section class="app-card">
-          <div class="app-title"> Command Line Org Chart Generator </div>
+          <div class="app-title"> NodeJS Team Card Generator </div>
           <section>
             <div class="app-img app-card-content">
-              img here
+            <img src={tcg} alt="team card generator screenshot" class="app-img"/>
             </div>
             <p class="light-text smaller-text">
-            A command line orgchart generator - to manage an employee database. It uses Node.js, Inquirer, and MySQL. (Since this program runs entirely in the terminal via node.js, redirect to repo)
+            A command line application - to record employee information from a software engineering team and generate an HTML file that displays their info. (since this program runs entirely in the terminal via node.js, redirect to repo)
             </p>
           </section>
         </section>
         </a>
+
       </div>
       <br></br>
       <div class="vertical-fade-reverse"></div>
