a0dccdb0682cd765bb8e87d13878bc9ec3218f15
xndcmd <xndcmd@gmail.com>
2022-12-22 13:19:38 -0800

    added profile pic + improved homepage intro area styling

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import spacesurfer from './space-surfer.png'
+import miniProfile from './miniProfile.jpg'
 import tcg from './img/teamcardgen.png'
 import figjam from './img/figjam.png'
 import gbooks from './img/booksearch.png'
@@ -15,11 +16,23 @@ export default function Home() {
       <br></br>
       <h1>Hello</h1>
       <br></br>
-      <p>My name is Xander.</p>
-      <p>I am a Full Stack Developer.</p>
-      <img src={spacesurfer} alt="spaceman surfing" />
 
+      <div className='intro center'>
+
+        <img id='profilepic' src={miniProfile} alt="Mini Profile Pic" />
+
+        <div className='intro-words'>
+          <p>My name is Xander.</p>
+          <p>I am a Full Stack Developer.</p>
+        </div>
+
+        {/* <img src={spacesurfer} alt="spaceman surfing" /> */}
+
+      </div>
+
+      <br></br>
       <p>Here are links to some of my gitHub projects:</p>
+
       <div class="flex">
 
         <a href="https://github.com/xnd0/CmdLine_OrgChart_12.1">
@@ -40,76 +53,76 @@ export default function Home() {
 
         {/* <a href="WEST Coast smoke and FIRE"> */}
         <a href="https://github.com/xnd0/FigJam">
-        <section class="app-card">
-          <div class="app-title"> Smoke and Fire Map </div>
-          <section>
-            <div class="app-img app-card-content">
-            <img src={figjam} alt="figjam app screenshot" class="app-img"/>
-            </div>
-            <p class="light-text">
-            See real time information on wildfires and air quality index displayed on a map. Built with JavaScript, CSS, HTML, Leaflet, NASA API
-            </p>
+          <section class="app-card">
+            <div class="app-title"> Smoke and Fire Map </div>
+            <section>
+              <div class="app-img app-card-content">
+                <img src={figjam} alt="figjam app screenshot" class="app-img" />
+              </div>
+              <p class="light-text">
+                See real time information on wildfires and air quality index displayed on a map. Built with JavaScript, CSS, HTML, Leaflet, NASA API
+              </p>
+            </section>
           </section>
-        </section>
         </a>
 
-         {/* <a href="https:// Easy Keys "> */}
+        {/* <a href="https:// Easy Keys "> */}
         <a href="https://github.com/xnd0/FigJam">
-        <section class="app-card">
-          <div class="app-title"> Easy Keys </div>
-          <section>
-            <div class="app-img app-card-content">
-            <img src={figjam} alt="figjam app screenshot" class="app-img"/>
-            </div>
-            <p class="light-text">
-            An ultra-lightweight touchscreen synth-piano web app:
-            </p>
+          <section class="app-card">
+            <div class="app-title"> Easy Keys </div>
+            <section>
+              <div class="app-img app-card-content">
+                <img src={figjam} alt="figjam app screenshot" class="app-img" />
+              </div>
+              <p class="light-text">
+                An ultra-lightweight touchscreen synth-piano web app:
+              </p>
+            </section>
           </section>
-        </section>
         </a>
 
         {/* <a href="https://supermarche-fromage-49994.herokuapp.com/"> */}
         <a href="https://github.com/xnd0/FigJam">
-        <section class="app-card">
-          <div class="app-title"> FigJam </div>
-          <section>
-            <div class="app-img app-card-content">
-            <img src={figjam} alt="figjam app screenshot" class="app-img"/>
-            </div>
-            <p class="light-text">
-            A website for musical people - signup, login, post, browse, listen, & watch user's favorite music tracks and videos.
-            </p>
+          <section class="app-card">
+            <div class="app-title"> FigJam </div>
+            <section>
+              <div class="app-img app-card-content">
+                <img src={figjam} alt="figjam app screenshot" class="app-img" />
+              </div>
+              <p class="light-text">
+                A website for musical people - signup, login, post, browse, listen, & watch user's favorite music tracks and videos.
+              </p>
+            </section>
           </section>
-        </section>
         </a>
 
         {/* <a href="https://vast-headland-20181.herokuapp.com/"> */}
         <a href="https://vast-headland-20181.herokuapp.com/">
-        <section class="app-card">
-          <div class="app-title"> Custom GoogleBooks API Search </div>
-          <section>
-            <div class="app-img app-card-content">
-            <img src={gbooks} alt="google booksearch screenshot" class="app-img"/>
-            </div>
-            <p class="light-text">
-            An app to search google books API - allows users to create an account to save titles for later. User must login to save books to their account.
-            </p>
+          <section class="app-card">
+            <div class="app-title"> Custom GoogleBooks API Search </div>
+            <section>
+              <div class="app-img app-card-content">
+                <img src={gbooks} alt="google booksearch screenshot" class="app-img" />
+              </div>
+              <p class="light-text">
+                An app to search google books API - allows users to create an account to save titles for later. User must login to save books to their account.
+              </p>
+            </section>
           </section>
-        </section>
         </a>
 
         <a href="https://fierce-wildwood-04718.herokuapp.com/">
-        <section class="app-card">
-          <div class="app-title"> Find Your Pour </div>
-          <section>
-            <div class="app-img app-card-content">
-            <img src={pour} alt="find your pour screenshot" class="app-img"/>
-            </div>
-            <p class="light-text smaller-text">
-            A website for wine professionals & enthusiasts - wineries may register and post information about their wines, to be displayed on the 'complete wine list'.
-            </p>
+          <section class="app-card">
+            <div class="app-title"> Find Your Pour </div>
+            <section>
+              <div class="app-img app-card-content">
+                <img src={pour} alt="find your pour screenshot" class="app-img" />
+              </div>
+              <p class="light-text smaller-text">
+                A website for wine professionals & enthusiasts - wineries may register and post information about their wines, to be displayed on the 'complete wine list'.
+              </p>
+            </section>
           </section>
-        </section>
         </a>
 
         {/* <a href="https://xnd0.github.io/Are_We_There_Yet_Adventure_App/"> */}
