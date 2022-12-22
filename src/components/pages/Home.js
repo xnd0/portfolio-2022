5a66f4c3f9bfa3aaa60014b6577f426bb14b1962
xndcmd <xndcmd@gmail.com>
2022-12-22 09:25:32 -0800

    changed some featured apps

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -22,24 +22,54 @@ export default function Home() {
       <p>Here are links to some of my gitHub projects:</p>
       <div class="flex">
 
-      <a href="https://github.com/xnd0/CmdLine_OrgChart_12.1">
+        <a href="https://github.com/xnd0/CmdLine_OrgChart_12.1">
+          <section class="app-card">
+            <div class="app-title"> Command Line Org Chart Generator </div>
+
+            <section>
+              <div class="app-img app-card-content">
+                <img src={orgchart} alt="orgchart generator screenshot" class="app-img" />
+              </div>
+              <p class="light-text">
+                A command line orgchart generator - to create and manage an employee database. It uses Node, Inquirer, and MySQL.
+              </p>
+
+            </section>
+          </section>
+        </a>
+
+        {/* <a href="WEST Coast smoke and FIRE"> */}
+        <a href="https://github.com/xnd0/FigJam">
         <section class="app-card">
-          <div class="app-title"> Command Line Org Chart Generator </div>
-          
+          <div class="app-title"> Smoke and Fire Map </div>
           <section>
             <div class="app-img app-card-content">
-            <img src={orgchart} alt="orgchart generator screenshot" class="app-img"/>
+            <img src={figjam} alt="figjam app screenshot" class="app-img"/>
             </div>
             <p class="light-text">
-            A command line orgchart generator - to create and manage an employee database. It uses Node, Inquirer, and MySQL.
+            See real time information on wildfires and air quality index displayed on a map. Built with JavaScript, CSS, HTML, Leaflet, NASA API
             </p>
-            
+          </section>
+        </section>
+        </a>
+
+         {/* <a href="https:// Easy Keys "> */}
+        <a href="https://github.com/xnd0/FigJam">
+        <section class="app-card">
+          <div class="app-title"> Easy Keys </div>
+          <section>
+            <div class="app-img app-card-content">
+            <img src={figjam} alt="figjam app screenshot" class="app-img"/>
+            </div>
+            <p class="light-text">
+            An ultra-lightweight touchscreen synth-piano web app:
+            </p>
           </section>
         </section>
         </a>
 
         {/* <a href="https://supermarche-fromage-49994.herokuapp.com/"> */}
-        <a href="https://github.com/xnd0/FigJam">
+        {/* <a href="https://github.com/xnd0/FigJam">
         <section class="app-card">
           <div class="app-title"> FigJam </div>
           <section>
@@ -51,10 +81,10 @@ export default function Home() {
             </p>
           </section>
         </section>
-        </a>
+        </a> */}
 
         {/* <a href="https://vast-headland-20181.herokuapp.com/"> */}
-        <a href="https://vast-headland-20181.herokuapp.com/">
+        {/* <a href="https://vast-headland-20181.herokuapp.com/">
         <section class="app-card">
           <div class="app-title"> Custom GoogleBooks API Search </div>
           <section>
@@ -66,9 +96,9 @@ export default function Home() {
             </p>
           </section>
         </section>
-        </a>
+        </a> */}
 
-        <a href="https://fierce-wildwood-04718.herokuapp.com/">
+        {/* <a href="https://fierce-wildwood-04718.herokuapp.com/">
         <section class="app-card">
           <div class="app-title"> Find Your Pour </div>
           <section>
@@ -80,35 +110,35 @@ export default function Home() {
             </p>
           </section>
         </section>
-        </a>
+        </a> */}
 
         {/* <a href="https://xnd0.github.io/Are_We_There_Yet_Adventure_App/"> */}
         <a href="https://github.com/xnd0/Are_We_There_Yet_Adventure_App">
-        <section class="app-card">
-          <div class="app-title"> Are We There Yet </div>
-          <section>
-            <div class="app-img app-card-content">
-            <img src={awty} alt="are we there yet screenshot" class="app-img"/>
-            </div>
-            <p class="light-text smaller-text">
-            An app for adventure - search by state to discover national parks and landmarks. Results are returned on the mainpage map. Save favorite places to remember for later.
-            </p>
+          <section class="app-card">
+            <div class="app-title"> Are We There Yet </div>
+            <section>
+              <div class="app-img app-card-content">
+                <img src={awty} alt="are we there yet screenshot" class="app-img" />
+              </div>
+              <p class="light-text smaller-text">
+                An app for adventure - search by state to discover national parks and landmarks. Results are returned on the mainpage map. Save favorite places to remember for later.
+              </p>
+            </section>
           </section>
-        </section>
         </a>
 
         <a href="https://github.com/xnd0/Team_Card_Generator_10.3">
-        <section class="app-card">
-          <div class="app-title"> NodeJS Team Card Generator </div>
-          <section>
-            <div class="app-img app-card-content">
-            <img src={tcg} alt="team card generator screenshot" class="app-img"/>
-            </div>
-            <p class="light-text smaller-text">
-            A command line application - to record employee information from a software engineering team and generate an HTML file that displays their info.
-            </p>
+          <section class="app-card">
+            <div class="app-title"> NodeJS Team Card Generator </div>
+            <section>
+              <div class="app-img app-card-content">
+                <img src={tcg} alt="team card generator screenshot" class="app-img" />
+              </div>
+              <p class="light-text smaller-text">
+                A command line application - to record employee information from a software engineering team and generate an HTML file that displays their info.
+              </p>
+            </section>
           </section>
-        </section>
         </a>
 
       </div>
